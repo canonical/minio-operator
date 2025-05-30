a4edbbc6d729483d20406a9f3ba9c49ed7766d15
Deezzir <deezzir@gmail.com>
2025-05-30 01:11:04 -0400

    chore: update unit test

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,62 +1,48 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 import json
-from base64 import b64decode
-from unittest.mock import MagicMock, PropertyMock
 
 import pytest
 import yaml
 from ops.model import ActiveStatus, BlockedStatus, WaitingStatus
 from ops.testing import Harness
 
-from charm import Operator
+from charm import MinIOOperator
+
+CONTAINER_NAME = "minio"
 
 
 @pytest.fixture
-def harness():
-    return Harness(Operator)
+def harness() -> Harness:
+    return Harness(MinIOOperator)
 
 
 def test_not_leader(harness):
+    """Test when we are not the leader."""
     harness.begin_with_initial_hooks()
-    assert harness.charm.model.unit.status == WaitingStatus("Waiting for leadership")
+    # Assert that we are not Active, and that the leadership-gate is the cause.
+    assert not isinstance(harness.charm.model.unit.status, ActiveStatus)
+    assert harness.charm.model.unit.status.message.startswith("[leadership-gate]")
 
 
-def test_missing_image(harness):
+def test_no_relation(harness):
+    """Test that the charm enters ActiveStatus if there is no relation."""
+    # Arrange
     harness.set_leader(True)
+
+    # Act
     harness.begin_with_initial_hooks()
-    assert harness.charm.model.unit.status == BlockedStatus("Missing resource: oci-image")
 
-
-def test_main_no_relation(harness):
-    harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
-    harness.begin_with_initial_hooks()
-    pod_spec = harness.get_pod_spec()
-
-    # confirm that we can serialize the pod spec
-    yaml.safe_dump(pod_spec)
-
+    # Assert
     assert harness.charm.model.unit.status == ActiveStatus("")
+    container = harness.charm.unit.get_container(CONTAINER_NAME)
+    assert container.get_service(CONTAINER_NAME).is_running()
 
 
-def test_incompatible_version(harness):
+def test_object_storage_relation_incompatible_version(harness):
+    """Test that the charm enters BlockedStatus if the object-storage relation is incompatible."""
+    # Arrange
     harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
     rel_id = harness.add_relation("object-storage", "argo-controller")
     harness.add_relation_unit(rel_id, "argo-controller/0")
     harness.update_relation_data(
@@ -64,38 +50,36 @@ def test_incompatible_version(harness):
         "argo-controller",
         {"_supported_versions": yaml.dump(["v2"])},
     )
+
+    # Act
     harness.begin_with_initial_hooks()
+
+    # Assert
     assert harness.charm.model.unit.status == BlockedStatus(
-        "No compatible object-storage versions found for apps: argo-controller"
+        "[relation:object_storage] No compatible object-storage versions found for apps: "
+        "argo-controller"
     )
 
 
-def test_unversioned(harness):
+def test_object_storage_relation_unversioned(harness):
+    """Test that the charm is in WaitingStatus if the object-storage relation is unversioned."""
+    # Arrange
     harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
     rel_id = harness.add_relation("object-storage", "argo-controller")
     harness.add_relation_unit(rel_id, "argo-controller/0")
+
+    # Act
     harness.begin_with_initial_hooks()
+
+    # Assert
     assert isinstance(harness.charm.model.unit.status, WaitingStatus)
 
 
-def test_main_with_relation(harness):
+def test_object_storage_relation(harness):
+    """Test that the object-storage relation is set up correctly."""
+    # Arrange
     harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
+
     rel_id = harness.add_relation("object-storage", "argo-controller")
     harness.add_relation_unit(rel_id, "argo-controller/0")
     harness.update_relation_data(
@@ -103,35 +87,35 @@ def test_main_with_relation(harness):
         "argo-controller",
         {"_supported_versions": yaml.dump(["v1"])},
     )
-    rel_id = harness.add_relation("object-storage", "foobar")
-    harness.add_relation_unit(rel_id, "foobar/0")
-    harness.update_relation_data(
-        rel_id,
-        "foobar",
-        {"_supported_versions": yaml.dump(["v1"])},
-    )
-    harness.begin_with_initial_hooks()
-    assert harness.charm.model.unit.status == ActiveStatus("")
 
+    # rel_id = harness.add_relation("object-storage", "foobar")
+    # harness.add_relation_unit(rel_id, "foobar/0")
+    # harness.update_relation_data(
+    #     rel_id,
+    #     "foobar",
+    #     {"_supported_versions": yaml.dump(["v1"])},
+    # )
+
+    # Act
+    harness.begin_with_initial_hooks()
+
+    # Assert
+    assert harness.charm.model.unit.status == ActiveStatus("")
     data = yaml.safe_load(harness.get_relation_data(rel_id, "minio")["data"])
+    secret_key = data["secret-key"]
     assert data["access-key"] == "minio"
-    assert data["namespace"] is None
     assert data["port"] == 9000
     assert data["secure"] is False
-    assert len(data["secret-key"]) == 30
+    assert len(secret_key) == 30
     assert data["service"] == "minio"
 
 
-def test_main_with_manual_secret(harness):
+def test_object_storage_relation_with_manual_secret(harness):
+    """Test that the object-storage relation is set up correctly with a manual secret key."""
+    # Arrange
     harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
+    harness.update_config({"secret-key": "test-key"})
+
     rel_id = harness.add_relation("object-storage", "argo-controller")
     harness.add_relation_unit(rel_id, "argo-controller/0")
     harness.update_relation_data(
@@ -139,14 +123,15 @@ def test_main_with_manual_secret(harness):
         "argo-controller",
         {"_supported_versions": yaml.dump(["v1"])},
     )
-    harness.begin_with_initial_hooks()
-    assert harness.charm.model.unit.status == ActiveStatus("")
 
-    harness.update_config({"secret-key": "test-key"})
+    # Act
+    harness.begin_with_initial_hooks()
+
+    # Assert
+    assert harness.charm.model.unit.status == ActiveStatus("")
     data = yaml.safe_load(harness.get_relation_data(rel_id, "minio")["data"])
     assert data == {
         "access-key": "minio",
-        "namespace": None,
         "port": 9000,
         "secret-key": "test-key",
         "secure": False,
@@ -156,25 +141,27 @@ def test_main_with_manual_secret(harness):
 
 
 def test_server_minio_args(harness):
+    """Test that the server minio args are set correctly."""
+    # Arrange
     harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
-    harness.update_config({"secret-key": "test-key"})
+    harness.update_config({"access-key": "minio", "secret-key": "test-key"})
+
+    # Act
     harness.begin_with_initial_hooks()
-    pod_spec = harness.get_pod_spec()
 
-    pod_spec_secrets = pod_spec[0]["kubernetesResources"]["secrets"]
-    pod_spec_secret_key = pod_spec_secrets[0]["data"]["MINIO_SECRET_KEY"]
-    pod_spec_secret_name = pod_spec_secrets[0]["name"]
+    # Assert
+    container = harness.charm.unit.get_container(CONTAINER_NAME)
+    assert container.get_service(CONTAINER_NAME).is_running()
+    command = container.get_plan().services[CONTAINER_NAME].command
+    environment = container.get_plan().services[CONTAINER_NAME].environment
 
-    assert b64decode(pod_spec_secret_key).decode("utf-8") == "test-key"
-    assert pod_spec[0]["containers"][0]["args"] == [
+    # Assert the environment variables are set correctly
+    assert environment["MINIO_ROOT_USER"] == "minio"
+    assert environment["MINIO_ROOT_PASSWORD"] == "test-key"
+    assert environment["MINIO_PROMETHEUS_AUTH_TYPE"] == "public"
+
+    # Assert the command includes the console address with the specified port
+    expected_args = [
         "server",
         "/data",
         "--certs-dir",
@@ -182,128 +169,161 @@ def test_server_minio_args(harness):
         "--console-address",
         ":9001",
     ]
-    assert pod_spec_secret_name == f"{harness.model.app.name}-secret"
+    assert command == f"minio {' '.join(expected_args)}"
 
 
 def test_gateway_minio_args(harness):
+    """Test that the gateway minio args are set correctly."""
+    # Arrange
     harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
     harness.update_config(
         {
+            "access-key": "minio",
             "secret-key": "test-key",
             "mode": "gateway",
             "gateway-storage-service": "azure",
         }
     )
+
+    # Act
     harness.begin_with_initial_hooks()
-    pod_spec = harness.get_pod_spec()
 
-    pod_spec_secrets = pod_spec[0]["kubernetesResources"]["secrets"]
-    pod_spec_secret_key = pod_spec_secrets[0]["data"]["MINIO_SECRET_KEY"]
+    # Assert
+    container = harness.charm.unit.get_container(CONTAINER_NAME)
+    assert container.get_service(CONTAINER_NAME).is_running()
+    command = container.get_plan().services[CONTAINER_NAME].command
 
-    assert b64decode(pod_spec_secret_key).decode("utf-8") == "test-key"
-    assert pod_spec[0]["containers"][0]["args"] == [
+    # Assert the command includes the console address with the specified port
+    expected_args = [
         "gateway",
         "azure",
         "--console-address",
         ":9001",
     ]
+    assert command == f"minio {' '.join(expected_args)}"
 
 
 def test_gateway_minio_missing_args(harness):
+    """Test that charm is blocked if required args for gateway mode are missing."""
+    # Arrange
     harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
     harness.update_config(
         {
             "secret-key": "test-key",
             "mode": "gateway",
         }
     )
+
+    # Act
     harness.begin_with_initial_hooks()
 
+    # Assert
     assert harness.charm.model.unit.status == BlockedStatus(
-        "Minio in gateway mode requires gateway-storage-service configuration. "
-        "Possible values: s3, azure"
+        "MinIO gateway mode requires 'gateway-storage-service' configuration. "
+        "Supported values: 's3', 'azure'"
+    )
+
+
+def test_invalid_minio_mode(harness):
+    """Test that charm is blocked if an invalid minio mode is set."""
+    # Arrange
+    harness.set_leader(True)
+    harness.update_config(
+        {
+            "access-key": "minio",
+            "secret-key": "test-key",
+            "mode": "invalid-mode",
+        }
+    )
+
+    # Act
+    harness.begin_with_initial_hooks()
+
+    # Assert
+    assert harness.charm.model.unit.status == BlockedStatus(
+        "Invalid mode 'invalid-mode'. Supported values: 'server', 'gateway'"
+    )
+
+
+def test_invalid_secret_key_length(harness):
+    """Test that charm is blocked if the secret key length is invalid."""
+    # Arrange
+    harness.set_leader(True)
+    harness.update_config(
+        {
+            "access-key": "minio",
+            "secret-key": "short",
+        }
+    )
+
+    # Act
+    harness.begin_with_initial_hooks()
+
+    # Assert
+    assert harness.charm.model.unit.status == BlockedStatus(
+        "The 'secret-key' config value must be at least 8 characters long."
     )
 
 
 def test_gateway_minio_with_private_endpoint(harness):
-    harness.set_leader(True)
-
-    secret_key = "test-key"
+    """Test that the gateway minio args are set correctly with a private endpoint."""
     minio_mode = "gateway"
     storage_service = "azure"
     storage_service_endpoint = "http://someendpoint"
 
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
+    # Arrange
+    harness.set_leader(True)
     harness.update_config(
         {
-            "secret-key": secret_key,
+            "access-key": "minio",
+            "secret-key": "test-key",
             "mode": minio_mode,
             "gateway-storage-service": storage_service,
             "storage-service-endpoint": storage_service_endpoint,
         }
     )
-    harness.begin_with_initial_hooks()
-    pod_spec = harness.get_pod_spec()
 
-    expected_container_args = [
+    # Act
+    harness.begin_with_initial_hooks()
+
+    # Assert
+    container = harness.charm.unit.get_container(CONTAINER_NAME)
+    assert container.get_service(CONTAINER_NAME).is_running()
+    command = container.get_plan().services[CONTAINER_NAME].command
+
+    # Assert the command includes the console address with the specified port
+    expected_args = [
         minio_mode,
         storage_service,
         storage_service_endpoint,
         "--console-address",
         ":9001",
     ]
-    assert pod_spec[0]["containers"][0]["args"] == expected_container_args
-
-    pod_spec_secrets = pod_spec[0]["kubernetesResources"]["secrets"]
-    pod_spec_secret_key = b64decode(pod_spec_secrets[0]["data"]["MINIO_SECRET_KEY"]).decode(
-        "utf-8"
-    )
-    assert pod_spec_secret_key == secret_key
+    assert command == f"minio {' '.join(expected_args)}"
 
 
 def test_minio_console_port_args(harness):
+    """Test that the console port is set correctly in the args."""
+    # Arrange
     harness.set_leader(True)
-    harness.add_oci_resource(
-        "oci-image",
-        {
-            "registrypath": "ci-test",
-            "username": "",
-            "password": "",
-        },
-    )
     harness.update_config(
         {
+            "access-key": "minio",
             "secret-key": "test-key",
             "console-port": 9999,
         }
     )
-    harness.begin_with_initial_hooks()
-    pod_spec = harness.get_pod_spec()
 
-    assert pod_spec[0]["containers"][0]["args"] == [
+    # Act
+    harness.begin_with_initial_hooks()
+
+    # Assert
+    container = harness.charm.unit.get_container(CONTAINER_NAME)
+    assert container.get_service(CONTAINER_NAME).is_running()
+    command = container.get_plan().services[CONTAINER_NAME].command
+
+    # Assert the command includes the console address with the specified port
+    expected_args = [
         "server",
         "/data",
         "--certs-dir",
@@ -311,89 +331,22 @@ def test_minio_console_port_args(harness):
         "--console-address",
         ":9999",
     ]
-
-
-@pytest.mark.parametrize(
-    "config,hash_salt,expected_hash",
-    [
-        (  # Standard working case
-            {"access-key": "access-key-value", "secret-key": "secret-key-value"},
-            "hash-salt",
-            "9b25665b7652ab845b909c718f6c66dccb0946a7ef8ddd607b85198d6deabe5f",
-        ),
-        (  # Vary the sorted order of keys
-            {
-                "x_last_alphabetical_order": "access-key-value",
-                "secret-key": "secret-key-value",
-            },
-            "hash-salt",
-            "a33e4682a38734508a9c8cb6971761636fefb0225eab0cb897443f1cf1317a07",
-        ),
-        (  # Vary the access-key
-            {"access-key": "access-key-value1", "secret-key": "secret-key-value"},
-            "hash-salt",
-            "162ba72393a4993626d553f2e64255f0998a70ef1b8ed4ea73652920d014898d",
-        ),
-        (  # Vary the salt
-            {"access-key": "access-key-value", "secret-key": "secret-key-value"},
-            "hash-salt1",
-            "82c0d902422d085cfc5d5d652d7ebd78175042705542fe7db9866a259bd06528",
-        ),
-    ],
-)
-# skipped because tests fail with ops 1.4
-# https://github.com/canonical/minio-operator/issues/58
-@pytest.mark.skip
-def test_generate_config_hash(config, hash_salt, expected_hash, harness):
-    ##################
-    # Setup test
-
-    harness.begin()
-
-    # Mock config to use a controlled subset of keys.  This avoids the expected hash changing
-    # whenever someone adds a new config option
-
-    # Use tuple(generator) instead of generator directly - if we use the bare generator directly
-    # it'll raise an exception in update_config because update_config will be editing the object
-    # we're taking keys() from
-    old_keys = tuple(harness.charm.config.keys())
-    harness.update_config(unset=old_keys)
-    assert len(harness.charm.config.keys()) == 0, "Failed to delete default config keys"
-
-    # Avoid triggering config_changed as hooks may have unexpected failures due to reduced config
-    # data
-    with harness.hooks_disabled():
-        harness.update_config(config)
-
-    # Mock away _stored with known values
-    harness.charm._stored = MagicMock()
-    mocked_salt = PropertyMock(return_value=hash_salt)
-    type(harness.charm._stored).hash_salt = mocked_salt
-
-    ##################
-    # Execute test
-
-    hashed_config = harness.charm._generate_config_hash()
-
-    ##################
-    # Check results
-    assert expected_hash == hashed_config
-
-
-# TODO: test get_secret_key
-# TODO: How can I test whether the hash/password gets randomly generated if respective config is
-#  omitted?  Or can/should I at all?
+    assert command == f"minio {' '.join(expected_args)}"
 
 
 def test_prometheus_data_set(harness, mocker):
+    """Test that the prometheus scrape jobs are set correctly in the relation data."""
+    # Arrange
     harness.set_leader(True)
     harness.set_model_name("kubeflow")
-    harness.begin()
 
+    # Act
+    harness.begin()
     rel_id = harness.add_relation("metrics-endpoint", "otherapp")
     harness.add_relation_unit(rel_id, "otherapp/0")
     harness.update_relation_data(rel_id, "otherapp", {})
 
+    # Assert
     assert json.loads(harness.get_relation_data(rel_id, harness.model.app.name)["scrape_jobs"])[0][
         "static_configs"
     ][0]["targets"] == ["*:9000"]
