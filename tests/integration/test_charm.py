d1729bd2607d2cfcaeb62106a26a51ec3a3826a1
Deezzir <deezzir@gmail.com>
2025-05-29 22:30:21 -0400

    chore: update integraion test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -20,14 +20,12 @@ from tenacity import Retrying, stop_after_delay, wait_exponential
 log = logging.getLogger(__name__)
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
-
+APP_NAME = METADATA["name"]
+CHARM_ROOT = "."
 MINIO_CONFIG = {
     "access-key": "minio",
     "secret-key": "minio-secret-key",
 }
-
-APP_NAME = "minio"
-CHARM_ROOT = "."
 
 
 @pytest.mark.abort_on_fail
@@ -35,8 +33,8 @@ async def test_build_and_deploy(ops_test: OpsTest):
     built_charm_path = await ops_test.build_charm(CHARM_ROOT)
     log.info(f"Built charm {built_charm_path}")
 
-    image_path = METADATA["resources"]["oci-image"]["upstream-source"]
-    resources = {"oci-image": image_path}
+    image_path = METADATA["resources"]["minio"]["upstream-source"]
+    resources = {"minio": image_path}
 
     await ops_test.model.deploy(
         entity_url=built_charm_path,
