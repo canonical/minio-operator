07238a4ed9796048596e3efec8dd43c5bec172de
Kimonas Sotirchos <kimonas.sotirchos@canonical.com>
2025-10-27 17:36:28 +0200

    feat: add OWASP logging for password change (#270)
    Closes #268
    
    I tried to also create a test that could capture the logs, and check if a line with owasp_event would be present. But I couldn't find exactly how to do this.
    
    This is something similar we did in the past, in case someone might have some ideas
    https://github.com/canonical/admission-webhook-operator/blob/5fbad5e336fbe899878a1ac5b14717a8b4ebe5ee/tests/unit/test_operator.py#L179-L181

---
+++ src/components/owasp_logging.py
@@ -0,0 +1,37 @@
+import logging
+
+from charmed_kubeflow_chisme.components import Component
+from ops import ActiveStatus, BoundStoredState, CharmBase, StatusBase
+from ops.charm import ConfigChangedEvent
+from owasp_logger import OWASPLogger
+
+logger = logging.getLogger(__name__)
+
+
+class OWASPLoggerComponent(Component):
+    def __init__(self, charm: CharmBase, stored: BoundStoredState, name="owasp-logger"):
+        super().__init__(charm, name)
+        self._charm = charm
+        self._owasp_logger = OWASPLogger(appid="minio.owasp-logger")
+        self._stored = stored
+        self._stored.set_default(last_secret_key_config="")
+
+    def _configure_app_leader(self, event):
+        if not isinstance(event, ConfigChangedEvent):
+            logger.info("Event is not a config-changed one. Skipping this component.")
+            return
+
+        logger.debug("Got a config-changed event. Will compare with previous value of secret-key")
+        config_secret = self.model.config.get("secret-key")
+        stored_secret = self._stored.last_secret_key_config
+
+        if config_secret != stored_secret:
+            logger.debug("Config value for secret-key has changed! Creating OWASP event.")
+            access_key = str(self.model.config.get("access-key"))
+            desc = f"The secret-key for access-key '{access_key}' was changed."
+            self._owasp_logger.authn_password_change(userid=access_key, description=desc)
+
+        self._stored.last_secret_key_config = config_secret
+
+    def get_status(self) -> StatusBase:
+        return ActiveStatus()
